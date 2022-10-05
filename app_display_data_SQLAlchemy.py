4d5d5139e646dc52027a3215f9bf290a9a5e056c
Gohil <gohildharmendrab@gmail.com>
2022-10-05 22:49:12 +0530

    Add/Display/Delete Data using SQLAlchemy

---
+++ app_display_data_SQLAlchemy.py
@@ -0,0 +1,50 @@
+'''
+Display Data using SQLAlchemy
+pip install flask-sqlalchemy
+python3 -m pip install PyMySQL[rsa]
+
+##For Passwod
+from urllib.parse import quote_plus
+python3 -m pip install PyMySQL[rsa] #for password using with @ in password
+
+'''
+
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote_plus
+import pymysql
+
+
+app = Flask(__name__)
+ip='localhost'
+username='admin'
+password= quote_plus("admin@123")
+db_name='flask_example'
+
+app.config['SQLALCHEMY_DATABASE_URI']=f'mysql+pymysql://{username}:{password}@{ip}/{db_name}'
+app.config['SECRET_KEY']='db@test'
+db = SQLAlchemy(app)
+
+class db_api_user(db.Model):
+    __tablename__ = 'db_api_user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)    
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+    def __repr__(self):
+        return '<db_api_user %r>' % self.username
+    
+    
+
+@app.route('/',)
+def read_data():   
+    data = db_api_user.query.all()  
+    return render_template('display.html',data=data)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()    
+    app.run(debug=True, port=8080)
---
+++ app_insert_data_SQLAlchemy.py
@@ -0,0 +1,64 @@
+'''
+Insert Data using SQLAlchemy 
+pip install flask-sqlalchemy
+python3 -m pip install PyMySQL[rsa]
+
+##For Passwod
+from urllib.parse import quote_plus
+python3 -m pip install PyMySQL[rsa] #for password using with @ in password
+
+'''
+
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote_plus
+import pymysql
+
+
+app = Flask(__name__)
+ip='localhost'
+username='admin'
+password= quote_plus("admin@123")
+db_name='flask_example'
+
+app.config['SQLALCHEMY_DATABASE_URI']=f'mysql+pymysql://{username}:{password}@{ip}/{db_name}'
+app.config['SECRET_KEY']='db@test'
+db = SQLAlchemy(app)
+
+class db_api_user(db.Model):
+    # __tablename__ = 'db_api_user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)    
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+    def __repr__(self):
+        return '<db_api_user %r>' % self.username
+    
+    
+
+@app.route('/', methods=['GET','POST'])
+def insert_data():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        api_model = db_api_user(username = name, email = email)
+        save_db = db.session
+        try:
+            save_db.add(api_model)
+            save_db.commit()
+            return("Add Data Successfully !!")
+        except:
+            save_db.rollback()
+            save_db.flush()
+            return("Error Occure")
+            
+            
+    return render_template('insert-data.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()    
+    app.run(debug=True, port=8080)
